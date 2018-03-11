e098a03ec4fc8d16b46163aa1e5888b9e4b1d946
Jkat <Fission@live.com>
2018-03-11 15:46:49 -0400

    error

--- network/mmxvi.py
+++ network/mmxvi.py
@@ -1,25 +1,16 @@
-
 from clove.network.bitcoin import Bitcoin
 
 
-class MMXVI(Bitcoin):
+class mMXVI(Bitcoin):
     """
     Class with all the necessary MMXVI network information based on
-    http://www.github.com/coin2016/MMXVI/blob/master/src/net.cpp
+    https://github.com/coin2016/mmxvi/blob/master/src/net.cpp
     (date of access: 02/12/2018)
     """
-    name = 'mmxvi'
+    name = 'MMXVI'
     symbols = ('MMXVI', )
-    seeds = ('5.196.67.100',)
+    seeds =  ("5.196.67.100")
     port = 6503
 
 
-class MMXVITestNet(MMXVI):
-    """
-    Class with all the necessary MMXVI testing network information based on
-    http://www.github.com/coin2016/MMXVI/blob/master/src/net.cpp
-    (date of access: 02/12/2018)
-    """
-    name = 'test-mmxvi'
-    seeds = ('dnsseed.MMXVI.org',)
-    port = 16503
+# Has no Testnet
